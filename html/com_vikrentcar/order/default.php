d661e1726a9e129c9f4393afa7167f3113297a40
aproxis <aproxis@users.noreply.github.com>
2026-05-06 19:04:28 +0300

    upd

--- html/com_vikrentcar/order/default.php
+++ html/com_vikrentcar/order/default.php
@@ -237,6 +237,13 @@ $document->addStyleSheet(JURI::root() . 'templates/rent/css/order-details-styles
 								</div>
 								<div class="order-car-detail">
 									<svg class="order-car-detail-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+										<path d="M21 10c0 6-9 13-9 13s-9-7-9-13a9 9 0 1 1 18 0z"></path>
+										<polyline points="12 2 12 12 16 16"></polyline>
+									</svg>
+									<span><?php echo VikRentCar::getPlaceName($ord['idplace'], $vrc_tn); ?></span>
+								</div>
+								<div class="order-car-detail">
+									<svg class="order-car-detail-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 										<rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
 										<line x1="16" y1="2" x2="16" y2="6"></line>
 										<line x1="8" y1="2" x2="8" y2="6"></line>
@@ -275,19 +282,13 @@ $document->addStyleSheet(JURI::root() . 'templates/rent/css/order-details-styles
 									</span>
 								</div>
 
+								
 								<div class="order-car-detail">
 									<svg class="order-car-detail-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
 										<path d="M21 10c0 6-9 13-9 13s-9-7-9-13a9 9 0 1 1 18 0z"></path>
 										<polyline points="12 2 12 12 16 16"></polyline>
 									</svg>
-									<span><?php echo JText::_('VRCPICKUP') ?: 'Pickup'; ?>: <?php echo VikRentCar::getPlaceName($ord['idplace'], $vrc_tn); ?></span>
-								</div>
-								<div class="order-car-detail">
-									<svg class="order-car-detail-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-										<path d="M21 10c0 6-9 13-9 13s-9-7-9-13a9 9 0 1 1 18 0z"></path>
-										<polyline points="12 2 12 12 16 16"></polyline>
-									</svg>
-									<span><?php echo JText::_('VRCRETURN') ?: 'Return' ?>: <?php echo VikRentCar::getPlaceName($ord['idreturnplace'], $vrc_tn); ?></span>
+									<span><?php echo VikRentCar::getPlaceName($ord['idreturnplace'], $vrc_tn); ?></span>
 								</div>
 							</div>
 						</div>
